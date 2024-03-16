5d6b798fbd10c19383d09ca834c4c3a5917e84b3
cqlyj <luoyingjie0721@gmail.com>
2024-03-16 13:08:53 +0800

    Implement checkpoint 2

--- packages/hardhat/contracts/DEX.sol
+++ packages/hardhat/contracts/DEX.sol
@@ -15,7 +15,8 @@ contract DEX {
 	/* ========== GLOBAL VARIABLES ========== */
 
 	IERC20 token; //instantiates the imported contract
-
+	uint256 public totalLiquidity;
+	mapping(address => uint256) liquidity;
 	/* ========== EVENTS ========== */
 
 	/**
@@ -70,7 +71,13 @@ contract DEX {
 	 * @return totalLiquidity is the number of LPTs minting as a result of deposits made to DEX contract
 	 * NOTE: since ratio is 1:1, this is fine to initialize the totalLiquidity (wrt to balloons) as equal to eth balance of contract.
 	 */
-	function init(uint256 tokens) public payable returns (uint256) {}
+	function init(uint256 tokens) public payable returns (uint256) {
+		totalLiquidity = address(this).balance;
+		token.transferFrom(msg.sender, address(this), tokens);
+		liquidity[msg.sender] = tokens;
+		totalLiquidity += tokens;
+		return totalLiquidity;
+	}
 
 	/**
 	 * @notice returns yOutput, or yDelta for xInput (or xDelta)
@@ -88,7 +95,9 @@ contract DEX {
 	 * NOTE: if you are using a mapping liquidity, then you can use `return liquidity[lp]` to get the liquidity for a user.
 	 * NOTE: if you will be submitting the challenge make sure to implement this function as it is used in the tests.
 	 */
-	function getLiquidity(address lp) public view returns (uint256) {}
+	function getLiquidity(address lp) public view returns (uint256) {
+		return liquidity[lp];
+	}
 
 	/**
 	 * @notice sends Ether to DEX in exchange for $BAL
