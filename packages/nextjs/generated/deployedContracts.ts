88f2f77e1a7d300e5e644ef53cc00a7f82ddce6b
cqlyj <luoyingjie0721@gmail.com>
2024-03-18 21:50:42 +0800

    Implement all checkpoints

--- packages/nextjs/generated/deployedContracts.ts
+++ packages/nextjs/generated/deployedContracts.ts
@@ -210,6 +210,217 @@ const contracts = {
       },
     },
   ],
+  11155111: [
+    {
+      chainId: "11155111",
+      name: "sepolia",
+      contracts: {
+        Streamer: {
+          address: "0xc3c5DC23983D84541760864D8B6e495d04b7E627",
+          abi: [
+            {
+              anonymous: false,
+              inputs: [
+                {
+                  indexed: false,
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              name: "Challenged",
+              type: "event",
+            },
+            {
+              anonymous: false,
+              inputs: [
+                {
+                  indexed: false,
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              name: "Closed",
+              type: "event",
+            },
+            {
+              anonymous: false,
+              inputs: [
+                {
+                  indexed: false,
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+                {
+                  indexed: false,
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              name: "Opened",
+              type: "event",
+            },
+            {
+              anonymous: false,
+              inputs: [
+                {
+                  indexed: true,
+                  internalType: "address",
+                  name: "previousOwner",
+                  type: "address",
+                },
+                {
+                  indexed: true,
+                  internalType: "address",
+                  name: "newOwner",
+                  type: "address",
+                },
+              ],
+              name: "OwnershipTransferred",
+              type: "event",
+            },
+            {
+              anonymous: false,
+              inputs: [
+                {
+                  indexed: false,
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+                {
+                  indexed: false,
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              name: "Withdrawn",
+              type: "event",
+            },
+            {
+              inputs: [],
+              name: "challengeChannel",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "defundChannel",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "fundChannel",
+              outputs: [],
+              stateMutability: "payable",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "owner",
+              outputs: [
+                {
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "renounceOwnership",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address",
+                  name: "channel",
+                  type: "address",
+                },
+              ],
+              name: "timeLeft",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address",
+                  name: "newOwner",
+                  type: "address",
+                },
+              ],
+              name: "transferOwnership",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  components: [
+                    {
+                      internalType: "uint256",
+                      name: "updatedBalance",
+                      type: "uint256",
+                    },
+                    {
+                      components: [
+                        {
+                          internalType: "bytes32",
+                          name: "r",
+                          type: "bytes32",
+                        },
+                        {
+                          internalType: "bytes32",
+                          name: "s",
+                          type: "bytes32",
+                        },
+                        {
+                          internalType: "uint8",
+                          name: "v",
+                          type: "uint8",
+                        },
+                      ],
+                      internalType: "struct Streamer.Signature",
+                      name: "sig",
+                      type: "tuple",
+                    },
+                  ],
+                  internalType: "struct Streamer.Voucher",
+                  name: "voucher",
+                  type: "tuple",
+                },
+              ],
+              name: "withdrawEarnings",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+          ],
+        },
+      },
+    },
+  ],
 } as const;
 
 export default contracts;
